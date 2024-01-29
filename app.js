aea2c4e295b8363b6202d4629f1a2d2ea97a69f1
aayushmehta02 <83535280+aayushmehta02@users.noreply.github.com>
2024-01-29 21:36:23 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+let currentIndex = 1;
+const totalCards = document.getElementsByClassName('card');
+totalCardsLength = 7
+
+const windowWidth = window.innerWidth;
+
+if(windowWidth < 606){
+    function updateCarousel() {
+        const wrapper = document.querySelector('.carousel-wrapper');
+        wrapper.style.transform = `translateX(${-currentIndex * (100 )}%)`; // Adjust the percentage based on the number of cards displayed
+    }
+    
+    function nextSlide() {
+        currentIndex = (currentIndex + 1) % (totalCardsLength - 2);
+        console.log(currentIndex) // Adjust the total number of cards to show 3 at a time
+        updateCarousel();
+    }
+    
+    function prevSlide() {
+        currentIndex = (currentIndex - 1 + (totalCardsLength - 2)) % (totalCardsLength - 2);
+        console.log(currentIndex)  // Adjust the total number of cards to show 3 at a time
+        updateCarousel();
+    }
+}else{
+    function updateCarousel() {
+        const wrapper = document.querySelector('.carousel-wrapper');
+        wrapper.style.transform = `translateX(${-currentIndex * (100 / 3)}%)`; // Adjust the percentage based on the number of cards displayed
+    }
+    
+    function nextSlide() {
+        currentIndex = (currentIndex + 1) % (totalCardsLength - 2);
+        console.log(currentIndex) // Adjust the total number of cards to show 3 at a time
+        updateCarousel();
+    }
+    
+    function prevSlide() {
+        currentIndex = (currentIndex - 1 + (totalCardsLength - 2)) % (totalCardsLength - 2);
+        console.log(currentIndex)  // Adjust the total number of cards to show 3 at a time
+        updateCarousel();
+    }
+}
+
+
+
+
+
