17426253ded6243f8c9ec2a9f6381907e09c65cc
aayushmehta02 <83535280+aayushmehta02@users.noreply.github.com>
2024-09-17 21:07:07 +0530

    added computer from glb

--- portfolio/src/components/HackerRoom.jsx
+++ portfolio/src/components/HackerRoom.jsx
@@ -1,9 +1,24 @@
+import { useGLTF } from '@react-three/drei'
 import React from 'react'
-
-const HackerRoom = () => {
+ const   HackerRoom=(props)=> {
+  const { nodes, materials } = useGLTF("assets/hacker.glb")
   return (
-    <div>HackerRooms</div>
+    <group {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.retro_computer_setup_retro_computer_setup_Mat_0.geometry}
+        material={materials.retro_computer_setup_Mat}
+        rotation={[-Math.PI / 2, 0, 0]}
+      />
+    </group>
   )
 }
+useGLTF.preload("assets/hacker.glb")
+
+
 
 export default HackerRoom
+
+
+
--- portfolio/src/sections/Hero.jsx
+++ portfolio/src/sections/Hero.jsx
@@ -1,11 +1,12 @@
+import { PerspectiveCamera, useFaceControls } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
-
-import { PerspectiveCamera } from '@react-three/drei';
-import React from 'react';
+import React, { Suspense } from 'react';
+import CanvasLoader from '../components/CanvasLoader';
 import HackerRoom from '../components/HackerRoom';
 
 
 const Hero = () => {
+    const controls = useFaceControls
   return (
     <section className='min-h-screen w-full flex flex-col relative'>
         <div className='w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3'>
@@ -15,9 +16,18 @@ const Hero = () => {
             <p className='hero_tag text-gray_gradient'> Building Products & Brands</p>
         </div>
         <div className='w-full h-full absolute inset-0'>
-            <Canvas className='w-full h-full'>
+            <Canvas className='w-full h-full' >
+            <Suspense fallback={<CanvasLoader/>}>
                 <PerspectiveCamera makeDefault position = {[0,0,30]}/>
-                <HackerRoom/>
+                <ambientLight intensity={1} />
+      <directionalLight position={[10, 10, 10]} intensity={0.5} />
+                <HackerRoom 
+  scale={0.1} 
+  position={[0, -7, 2]} 
+  rotation={[0, -Math.PI / 4, 0]} 
+/>
+
+            </Suspense>
             </Canvas>
         </div>
     </section>
