17aeb7b5b0c13fc6b6773af6706f9d3cae7fd460
aayushmehta02 <83535280+aayushmehta02@users.noreply.github.com>
2024-10-08 01:27:32 +0530

    added screen to the glb

--- portfolio/src/components/DemoComputer.jsx
+++ portfolio/src/components/DemoComputer.jsx
@@ -1,14 +1,16 @@
-import { useGLTF } from '@react-three/drei';
+import { useGLTF, useTexture } from '@react-three/drei';
 import { useFrame, useThree } from '@react-three/fiber';
 import React, { useRef } from 'react';
-
 export function DemoComputer(props) {
   const { nodes, materials } = useGLTF('/assets/macbook_pro_2021.glb');
   const groupRef = useRef();
   
   // Get the mouse position from useThree inside the component
   const { mouse } = useThree();
-
+  const screenTexture = useTexture('/assets/framer.png');
+  //scaling the size of the image within the screen
+  screenTexture.repeat.set(0.25, 0.25);  // Reduces the image to 50% of its original size
+  screenTexture.offset.set(0.25, 0.25);
   // Rotation based on mouse movement
   useFrame(() => {
     if (groupRef.current) {
@@ -29,28 +31,35 @@ export function DemoComputer(props) {
   return (
     <group {...props} dispose={null} ref={groupRef} >
       {/* Applying default rotation to correct the initial 90-degree rotation */}
-      <group rotation={[0, Math.PI / 2, 0]} position={[0.1212586, 0.0073169, -0.09  ]}>
+      <group rotation={[0, Math.PI / 2, 0]} position={[0.1212586, -0.1, -0.09  ]} scale={1.8}>
         <mesh
           castShadow
           receiveShadow
           geometry={nodes.Object_6.geometry}
           material={materials.MacBookPro}
-        />
+        >
+          <meshStandardMaterial
+          attach="material"
+          transparent = {true}
+           map={screenTexture} />
+        </mesh>
         <mesh
           castShadow
           receiveShadow
           geometry={nodes.Object_8.geometry}
           material={materials.MacBookPro}
         />
+          
+        
       </group>
-      <group rotation={[0, Math.PI / 2, 0]} position={[0.1212586, 0.0073169, 0]}>
+      {/* <group rotation={[0, Math.PI / 2, 0]} position={[0.1212586, 0.0073169, 0]}>
       <mesh
         castShadow
         receiveShadow
         geometry={nodes.Object_4.geometry}
         material={materials.MacBookPro}
       />
-      </group>
+      </group> */}
       
     </group>
   );
